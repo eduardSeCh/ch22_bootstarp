d8625c5600998430992d5dbc9f78fc9f3729b608
Eduard <eduardogsec@gmail.com>
2023-01-10 13:10:19 -0600

    [CI] gif to say hello

--- practicas/holaNombre/holaNombre.js
+++ practicas/holaNombre/holaNombre.js
@@ -2,4 +2,5 @@
 function changeName() {
     const name = prompt('Tu nombre');
     document.getElementById("changeNames").innerHTML = name;
+    document.getElementsByClassName("tenor-gif-embed")[0].style.opacity = '1';
 }
